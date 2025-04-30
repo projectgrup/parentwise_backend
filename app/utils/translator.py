9de1bf9a4f29b979419b7c46ebb214163c71cacd
projectgrup <varsharoy041@gmail.com>
2025-04-30 11:36:55 +0530

    Update translator.py

--- app/utils/translator.py
+++ app/utils/translator.py
@@ -1,16 +1,11 @@
 from langdetect import detect
 from deep_translator import GoogleTranslator
 
-def translate_text(text, target_lang):
-    try:
-        if not text.strip():
-            return text
-        src_lang = detect(text)
-        if src_lang == target_lang:
-            return text
-        return GoogleTranslator(source=src_lang, target=target_lang).translate(text)
-    except Exception as e:
-        print(f"[Translation Error] {e}")
+def translate_text(text: str, target_lang: str):
+    src = detect(text)
+    if src == target_lang:
         return text
+    return GoogleTranslator(source=src, target=target_lang).translate(text)
+
 
 
